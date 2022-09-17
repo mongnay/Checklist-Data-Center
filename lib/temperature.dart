94176cfee14e570f99c77060dfcd52cb44af5a1f
Riee <77129898+mongnay@users.noreply.github.com>
2022-09-17 17:22:08 +0700

    database write fixed

--- lib/temperature.dart
+++ lib/temperature.dart
@@ -1,6 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class temperMenu extends StatelessWidget {
+  final suhuRNI = TextEditingController();
+  final humidityRNI = TextEditingController();
+  final suhuRA = TextEditingController();
+  final humidityRA = TextEditingController();
+  final suhuRF = TextEditingController();
+  final humidityRD = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +27,7 @@ class temperMenu extends StatelessWidget {
                 Container(
                     width: 55,
                     child: TextField(
+                      controller: suhuRNI,
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
                       ),
@@ -33,6 +42,7 @@ class temperMenu extends StatelessWidget {
                 Container(
                     width: 55,
                     child: TextField(
+                      controller: humidityRNI,
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
                       ),
@@ -46,6 +56,7 @@ class temperMenu extends StatelessWidget {
                 Container(
                     width: 55,
                     child: TextField(
+                      controller: suhuRA,
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
                       ),
@@ -59,6 +70,7 @@ class temperMenu extends StatelessWidget {
                 Container(
                     width: 55,
                     child: TextField(
+                      controller: humidityRA,
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
                       ),
@@ -72,6 +84,7 @@ class temperMenu extends StatelessWidget {
                 Container(
                     width: 55,
                     child: TextField(
+                      controller: suhuRF,
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
                       ),
@@ -85,6 +98,7 @@ class temperMenu extends StatelessWidget {
                 Container(
                     width: 55,
                     child: TextField(
+                      controller: humidityRD,
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
                       ),
@@ -92,7 +106,23 @@ class temperMenu extends StatelessWidget {
               ]),
             ),
             MaterialButton(
-              onPressed: () {},
+              onPressed: () {
+                final suhu_rack_network_internal = suhuRNI.text;
+                final humidity_rack_network_internal = humidityRNI.text;
+                final suhu_rack_a = suhuRA.text;
+                final humidity_rack_a = humidityRA.text;
+                final suhu_rack_f = suhuRF.text;
+                final humidity_rack_f = humidityRD.text;
+
+                createTemperature(
+                    suhu_rack_network_internal: suhu_rack_network_internal,
+                    humidity_rack_network_internal:
+                        humidity_rack_network_internal,
+                    suhu_rack_a: suhu_rack_a,
+                    humidity_rack_a: humidity_rack_a,
+                    suhu_rack_f: suhu_rack_f,
+                    humidity_rack_f: humidity_rack_f);
+              },
               color: Color(0xff3531b9),
               elevation: 0,
               shape: RoundedRectangleBorder(
@@ -116,4 +146,58 @@ class temperMenu extends StatelessWidget {
       ),
     );
   }
+
+  void createTemperature(
+      {required String suhu_rack_network_internal,
+      humidity_rack_network_internal,
+      suhu_rack_a,
+      humidity_rack_a,
+      suhu_rack_f,
+      humidity_rack_f}) async {
+    final docTemperature =
+        FirebaseFirestore.instance.collection('temperature').doc();
+
+    final temp = Temp(
+      suhu_rack_network_internal: suhu_rack_network_internal,
+      humidity_rack_network_internal: humidity_rack_network_internal,
+      suhu_rack_a: suhu_rack_a,
+      humidity_rack_a: humidity_rack_a,
+      suhu_rack_f: suhu_rack_f,
+      humidity_rack_f: humidity_rack_f,
+      date: DateTime.now(),
+    );
+    final json = temp.toJson();
+
+    await docTemperature.set(json);
+  }
+}
+
+class Temp {
+  final String suhu_rack_network_internal;
+  final String humidity_rack_network_internal;
+  final String suhu_rack_a;
+  final String humidity_rack_a;
+  final String suhu_rack_f;
+  final String humidity_rack_f;
+  final DateTime date;
+
+  Temp({
+    required this.suhu_rack_network_internal,
+    required this.humidity_rack_network_internal,
+    required this.suhu_rack_a,
+    required this.humidity_rack_a,
+    required this.suhu_rack_f,
+    required this.humidity_rack_f,
+    required this.date,
+  });
+
+  Map<String, dynamic> toJson() => {
+        'suhu_rack_network_internal': suhu_rack_network_internal,
+        'humidity_rack_network_internal': humidity_rack_network_internal,
+        'suhu_rack_a': suhu_rack_a,
+        'humidity_rack_a': humidity_rack_a,
+        'suhu_rack_f': suhu_rack_f,
+        'humidity_rack_f': humidity_rack_f,
+        'date': date,
+      };
 }
